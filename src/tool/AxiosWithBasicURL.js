aec083b2503d9da9d72ff40ffda49da2084a74ad
zzy348429898 <348429898@qq.com>
2019-05-23 15:56:34 +0800

    update

--- src/tool/AxiosWithBasicURL.js
+++ src/tool/AxiosWithBasicURL.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+console.log(process.env.BASIC_URL, '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 export default axios.create({
   baseURL: process.env.BASIC_URL
 });
